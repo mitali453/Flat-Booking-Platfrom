76a59086842869e1859d814ae033fd103ab8735b
mitali453 <varshneymitali8122gmail.com>
2021-09-29 21:17:50 +0530

    change in message to check

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="bg-gray-700 text-white font-extrabold text-4xl">
-      hello world
+      This is our flat booking app
     </div>
   );
 }
