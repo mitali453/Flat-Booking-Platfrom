619a12e0052b0469e1646fe38c7fd7de293f3d31
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-10-05 22:02:21 +0530

    components divided

---
+++ src/Components/HomeComponents/Xyz.tsx
@@ -0,0 +1,11 @@
+import { FC, memo } from "react";
+interface Props {
+}
+const Xyz: FC<Props> = (props) => {
+    return (
+        <></>
+    );
+};
+Xyz.defaultProps = {
+}
+export default memo(Xyz);
