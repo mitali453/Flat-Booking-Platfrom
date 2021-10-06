dea316a039e3013f36d09b0ccb876fe0076b25f3
mitali453 <varshneymitali8122gmail.com>
2021-10-06 23:25:23 +0530

    created signup page

---
+++ src/SignupPage.tsx
@@ -0,0 +1,12 @@
+import {FC, memo} from "react";
+interface Props{
+}
+const SignupPage: FC<Props> = (props) =>{
+return (
+ <>This is Signup page .</>
+);
+};
+SignupPage.defaultProps={
+}
+export {};
+export default memo(SignupPage);
