aa418ff474f0f43f043af05b5e33cb3447b48a76
mitali453 <varshneymitali8122gmail.com>
2021-10-06 23:22:39 +0530

    Routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React  from 'react';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
 
-function App(){
-    return(
-        <div className=" bg-gray-500 text-blue-400 text-5xl">This is App</div>
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage></HomePage>
+                </Route>
+                <Route path="/login">
+                    <LoginPage></LoginPage>
+                </Route>
+                <Route path="/signup">
+                    <SignupPage></SignupPage>
+                </Route>
+
+                    
+            </Switch>
+            </BrowserRouter>
+        </div>
+
     );
 }
+//End of 
 export default App;
