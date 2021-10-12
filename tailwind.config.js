5b5a97023ef1bd7a3050a3d84e99e1e0ba4441a9
mitali453 <varshneymitali8122gmail.com>
2021-10-12 22:36:42 +0530

    added  new bg-color and text-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,39 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+
+      fontFamily: {
+    
+        'display': [  "-apple-system" , "BlinkMacSystemFont" , "Segoe UI" , "Roboto ", "Helvetica Neue" , "Arial" , "Noto Sans" , "sans-serif" , "Apple Color Emoji" , "Segoe UI Emoji" , "Segoe UI Symbol" , "Noto Color Emoji"],
+       },
+      textColor: {
+        yellow: {
+         
+          DEFAULT: '#b69100',
+         
+        }
+    },
+    backgroundColor: theme => ({
+      
+      'primary': '#b69100',
+     
+     }),
+      
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      },
+     
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+}}
