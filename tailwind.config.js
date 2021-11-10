aa50bae443c1bf9bdfff7194033a7b1b966643d0
mitali453 <mitali.varshney_cs19@gla.ac.in>
2021-11-10 22:17:50 +0530

    added bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
     backgroundImage: {
-        'hero': "url('https://images.pexels.com/photos/8143671/pexels-photo-8143671.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900')"
+       
       },
       textColor: {
         yellow: {
