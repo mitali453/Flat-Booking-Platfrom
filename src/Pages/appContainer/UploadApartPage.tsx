5730ce3ed045c372f11d8aaa72a434bbad83541f
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-11-10 19:47:46 +0530

    upload apartment added

---
+++ src/Pages/appContainer/UploadApartPage.tsx
@@ -0,0 +1,95 @@
+import { FC, memo } from "react";
+import { Link } from "react-router-dom";
+interface Props {
+}
+const UploadApartPage: FC<Props> = (props) => {
+    return (
+        <div className="flex mx-auto">
+            <section className="max-w-4xl p-6 mx-auto bg-gray-500 rounded-md shadow-md dark:bg-gray-800 mt-10">
+                <h1 className="text-xl font-bold text-white capitalize dark:text-white">Upload Apartment Details</h1>
+                <form>
+                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Name of Apartment</label>
+                            <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Full Name of Owner</label>
+                            <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Full Address</label>
+                            <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Contact Number</label>
+                            <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >No. of Floors</label>
+                            <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Select Area</label>
+                            <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" >
+                                <option>ALL AREAS</option>
+                                <option>New Village</option>
+                                <option>Old Town</option>
+                                <option>New City</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Price Range of Flats</label>
+                            <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" >
+                                <option>ALL RANGE</option>
+                                <option>RS 1000-2500</option>
+                                <option>RS 2500-5000</option>
+                                <option>RS 5000-10000</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label className="text-white dark:text-gray-200" >Description</label>
+                            <textarea id="textarea" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium text-white">
+                                Images
+                            </label>
+                            <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                <div className="space-y-1 text-center">
+                                    <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    <div className="flex text-sm text-gray-600">
+                                        <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                            <span className="">Upload a file</span>
+                                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                        </label>
+                                        <p className="pl-1 text-white">or drag and drop</p>
+                                    </div>
+                                    <p className="text-xs text-white">
+                                        PNG, JPG, GIF up to 10MB
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-end mt-6">
+                        <Link to="/DashBoard"><span className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-yellow-500 rounded-md hover:bg-yellow-700 focus:outline-none focus:bg-gray-600"><button>Upload & Submit</button></span> </Link>
+                    </div>
+                </form>
+            </section>
+        </div>
+    );
+};
+UploadApartPage.defaultProps = {
+}
+export default memo(UploadApartPage);
