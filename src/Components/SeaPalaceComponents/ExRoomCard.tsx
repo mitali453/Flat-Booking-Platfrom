17e2a1919232a8c83dcf39cdea2b1e8e55a11811
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2021-11-10 22:42:53 +0530

    final commits

--- src/Components/SeaPalaceComponents/ExRoomCard.tsx
+++ src/Components/SeaPalaceComponents/ExRoomCard.tsx
@@ -4,24 +4,7 @@ interface Props {
 const ExRoomCard: FC<Props> = (props) => {
     return (
         <>
-            <div className="lg:w-1/3 px-12 md:px-4 mr-auto ml-auto -mt-78 ">
-                <div className="flex  flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white">
-                    <div className="  relative  ">
-                        <img className="w-fullalign-middle rounded-t-lg" src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" alt="..." />
-                        <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute bottom-0">
-                            <polygon points="-30,95 583,95 583,65" className="text-white fill-current"></polygon>
-                        </svg>
-                    </div>
-                    <blockquote className="mb-2">
-                        <h4 className="text-xl font-bold px-8 pt-8 text-black">
-                            CLASSIC BEDROOM
-                        </h4>
-                        <p className="text-md font-light  px-8 pb-8 mt-2 text-black">
-                            Lorem ipsum dolor sit amet consectetur adipisicingea voluptatem, unde! Quasi quo, inventore enim consequuntur doloremque iusto tempore cum a dolores ad exercitationem mollitia ipsam? Illo expl deserunt.
-                        </p>
-                    </blockquote>
-                </div>
-            </div>
+            
         </>
     );
 };
--- src/Pages/appContainer/ProfilePage.tsx
+++ src/Pages/appContainer/ProfilePage.tsx
@@ -9,22 +9,22 @@ const ProfilePage: FC<Props> = (props) => {
                     <img className="w-24 h-24 sm:w-40 sm:h-40 md:w-72 md:h-72  rounded-full mx-auto" src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="chauhan" />
                 </div>
                 <div className="">
-                    <h3 className="text-center text-2xl sm:text-3xl text-gray-900 font-medium leading-8">Ayush Chauhan</h3>
+                    <h3 className="text-center text-2xl sm:text-3xl text-gray-900 font-medium leading-8">Dennis Jenner</h3>
                     <div className="text-center text-gray-500 text-sm sm:text-lg pt-2 font-semibold">
-                        <p>Customer</p>
+                        <p>Website Owner</p>
                     </div>
                     <table className="mx-auto text-sm sm:text-lg my-8">
                         <tr className="">
                             <td className="px-2 py-4 text-gray-500 font-semibold">Address</td>
-                            <td className="px-2 py-4">Bahjoi , Sambhal (UP)</td>
+                            <td className="px-2 py-4">GLA University, Mathura</td>
                         </tr>
                         <tr className="">
                             <td className="px-2 py-4 text-gray-500 font-semibold">Phone</td>
-                            <td className="px-2 py-4">+91 8923077164</td>
+                            <td className="px-2 py-4">+91 9999999999</td>
                         </tr>
                         <tr className="">
                             <td className="px-2 py-4 text-gray-500 font-semibold">Email</td>
-                            <td className="px-2 py-4">chauhanayush531@gmail.com</td>
+                            <td className="px-2 py-4">abc@gmail.com</td>
                         </tr>
                     </table>
 
